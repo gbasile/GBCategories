006b6205455ef4f33974588759be2f91acc7810d
Giuseppe Basile <pepe@sharitapp.com>
2012-05-14 15:43:13 +0200

    Check against nil before to insert an object into a dictionary

--- GBCategories/NSError+convenience.m
+++ GBCategories/NSError+convenience.m
@@ -11,16 +11,34 @@
 
 + (NSError *) errorWithError:(NSError *)error domain:(NSString *)newDomain errorCode:(NSInteger)newCode
 {
+	if (![error isKindOfClass:[NSError class]]) {
+		return nil;
+	}
+	if (![newDomain isKindOfClass:[NSString class]]) {
+		return error;
+	}
 	NSMutableDictionary *userInfoDict = [NSMutableDictionary dictionary];
-	[userInfoDict setObject:[error localizedDescription] forKey:NSLocalizedDescriptionKey];
-	[userInfoDict setObject:[error localizedFailureReason] forKey:NSLocalizedFailureReasonErrorKey];
-	[userInfoDict setObject:[error localizedRecoverySuggestion] forKey:NSLocalizedRecoveryOptionsErrorKey];
-
+	if ([error localizedDescription]) {
+		[userInfoDict setObject:[error localizedDescription] forKey:NSLocalizedDescriptionKey];
+	}
+	if ([error localizedFailureReason]) {
+		[userInfoDict setObject:[error localizedFailureReason] forKey:NSLocalizedFailureReasonErrorKey];
+	}
+	if ([error localizedRecoverySuggestion]) {
+		[userInfoDict setObject:[error localizedRecoverySuggestion] forKey:NSLocalizedRecoveryOptionsErrorKey];
+	}
 	return [NSError errorWithDomain:newDomain code:newCode userInfo:userInfoDict];
 }
 
 + (NSError *) errorWithError:(NSError *)error localizedDescription:(NSString *)localizedString
 {
+	if (![localizedString isKindOfClass:[NSString class]]) {
+		return error;
+	}
+	
+	if (![error isKindOfClass:[NSError class]]) {
+		return nil;
+	}
 	NSMutableDictionary *userInfoDict = [NSMutableDictionary dictionaryWithDictionary: [error userInfo]];
 	[userInfoDict setObject:localizedString forKey:NSLocalizedDescriptionKey];
 	return [NSError errorWithDomain:[error domain] code:[error code] userInfo:userInfoDict];
